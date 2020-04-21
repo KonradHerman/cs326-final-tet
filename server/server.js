709df2cd72b7cf0468aa71d5e39a03a5f9fba7b8
Peanut <ksomani@umass.edu>
2020-04-21 19:00:39 -0400

    .

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function (err, db) {
 	if (err) throw err;
-	console.log("Database created!");
-	db.close();
+	var dbo = db.db("mydb");
+	var myobj = { name: "Company Inc", address: "Highway 37" };
+	dbo.collection("customers").insertOne(myobj, (err, res) => {
+		if (err) throw err;
+		console.log("1 document inserted");
+		db.close();
+	});
 });
