730779de7a7474b10e1c5db682f38e9a0a74718a
Peanut <ksomani@umass.edu>
2020-05-02 18:27:43 -0400

    Update myserver-post.js

--- myserver-post.js
+++ myserver-post.js
@@ -332,8 +332,8 @@ var MyServer = /** @class */ (function () {
                     case 0: return [4 /*yield*/, this.games.getAll()];
                     case 1:
                         games = _a.sent();
-                        console.log(games.data);
-                        response.write(JSON.stringify({ result: "read", games: games.data }));
+                        console.log(games);
+                        response.write(JSON.stringify({ result: "read", games: games }));
                         response.end();
                         return [2 /*return*/];
                 }
