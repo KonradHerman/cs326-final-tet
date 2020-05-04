e6f465bf1c445be89cd3417a1ba209e970c444af
Konrad <k.radikal@gmail.com>
2020-05-04 12:56:39 -0400

    Update client.js

--- html/client.js
+++ html/client.js
@@ -80,6 +80,7 @@ function gameUpdate() {
 		// let drop = document.getElementById("gameSelect");
 		// let gameID = drop.find('option:selected').attr('id');
 		let gameID = $("gameSelect").find('option:selected').attr('id');
+		console.log(gameID);
 		let userID = document.getElementById("userid").value;
 		let own = document.getElementById("own").checked;
 		let add = document.getElementById("add").checked;
