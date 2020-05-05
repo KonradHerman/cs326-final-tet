8d2830dfbf809a05910e1c72ee8aeae8f2e78af1
Peanut <ksomani@umass.edu>
2020-05-05 01:10:18 -0400

    Update client.js

--- html/client.js
+++ html/client.js
@@ -249,7 +249,7 @@ function usersSearch() {
 		if (j["result"] !== "error") {
 			document.getElementById("searchuseroutput").innerHTML +=
 				'<a href="#" class="list-group-item list-group-item-action flex-column align-items-start active"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">' +
-				j.game.name +
+				JSON.parse(j.game).name +
 				'</h5><small>3 days ago</small></div><p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p><small>Donec id elit non mi porta.</small></a>';
 		} else {
 			console.log("failure to read all");
