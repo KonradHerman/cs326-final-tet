63c349b7991cff0fbd47ceaba31b3cd447d252fd
Konrad <k.radikal@gmail.com>
2020-05-05 15:30:11 -0400

    obj to arr

--- myserver-post.js
+++ myserver-post.js
@@ -143,7 +143,7 @@ var MyServer = /** @class */ (function () {
         return __awaiter(this, void 0, void 0, function () {
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4 /*yield*/, this.getEmails(request, response)];
+                    case 0: return [4 /*yield*/, this.getEmails(request.body.names, response)];
                     case 1:
                         _a.sent();
                         return [2 /*return*/];
