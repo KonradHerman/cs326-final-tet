73890e71655a21d08358eb15c4820ca69194e0da
Douglas-Silverman <53188336+Douglas-Silverman@users.noreply.github.com>
2020-05-06 14:07:01 -0400

    read game works, trying to output users who own

--- html/client.js
+++ html/client.js
@@ -246,6 +246,10 @@ function usersSearch() {
 		const j = await resp.json();
 		console.log(j.game);
 		if (j["result"] !== "error") {
+			for(let i = 0; i < j["game"]["own"].length; ++i){
+				document.getElementById("searchuseroutput").innerHTML +=
+				"<a>" + j["game"]["own"][i] + "</a><br>";
+			}
 			document.getElementById("searchuseroutput").innerHTML +=
 				'<a href="#" class="list-group-item list-group-item-action flex-column align-items-start active"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">List group item heading</h5><small>3 days ago</small></div><p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p><small>Donec id elit non mi porta.</small></a>';
 		} else {
